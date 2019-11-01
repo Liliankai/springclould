de8279976b60521801d79741334bdc1f9cdc96d8
Li Liankai <liliankai@htyunlu.com>
2019-11-01 11:33:09 +0800

    ribbon和openfeign分别练习使用hystrix熔断器

--- service-feign/src/main/java/com/springcloudtest/service/ServiceFeignService.java
+++ service-feign/src/main/java/com/springcloudtest/service/ServiceFeignService.java
@@ -1,5 +1,6 @@
 package com.springcloudtest.service;
 
+import com.springcloudtest.fallback.ServiceHiHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by liliankai on 2019/10/31.
  */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi", fallback = ServiceHiHystrix.class)
 public interface ServiceFeignService {
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
--- service-ribbon/src/main/java/com/springclouldtest/service/ServiceRibbonService.java
+++ service-ribbon/src/main/java/com/springclouldtest/service/ServiceRibbonService.java
@@ -1,5 +1,6 @@
 package com.springclouldtest.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,13 @@ public class ServiceRibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "errorMsg")
     public String hiService(String name) {
         return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
+    }
+
+    public String errorMsg(String name){
+        return "sorry" + name + "接口错误！";
     }
 
 }

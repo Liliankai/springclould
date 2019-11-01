53fb843a681131410d0db8868cda0e7df2d81421
Li Liankai <liliankai@htyunlu.com>
2019-11-01 14:20:10 +0800

    领用zuul实现路由和过滤

---
+++ service-zuul/src/test/java/com/springcloudtest/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
